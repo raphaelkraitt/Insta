775df098e4c988f903b043517045548d0257113e
raphaelkraitt <raphaelkrait2@gmail.com>
2025-11-27 18:48:06 +0200

    railway

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -44,7 +44,7 @@ const startServer = async () => {
     AuctionService.init(io);
     AuctionScheduler.start();
 
-    httpServer.listen(PORT, () => {
+    httpServer.listen(Number(PORT), '0.0.0.0', () => {
         console.log(`Server is running on port ${PORT}`);
     });
 };
